c34b22be20b5fa9b159800f3532cb213f9334f34
Leandro Lima <leandro.lima@oowlish.com>
2022-08-03 21:35:33 -0300

    making objects on the bucket public and adding content type

--- downloader.ts
+++ downloader.ts
@@ -5,6 +5,7 @@ import ytdl from "ytdl-core";
 export async function handler(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
     const body = JSON.parse(event.body!);
     const videoUrl = body.url;
+    
     try {
         const videoInfo = await ytdl.getBasicInfo(videoUrl);
         const videoStream = await ytdl(videoUrl);
@@ -12,13 +13,16 @@ export async function handler(event: APIGatewayEvent): Promise<APIGatewayProxyRe
         const s3Client = new AWS.S3();
 
         const result = await s3Client.upload({
-            Bucket: "youtube-video-downloader-dev-videosbucket-1byvd72tee8vq",
-            Key: `${videoInfo.videoDetails.title}.mp4`,
+            Bucket: process.env.VIDEOS_BUCKET_NAME!,
+            Key: `${videoInfo.videoDetails.title}-${Date.now()}.mp4`,
             Body: videoStream
         }).promise();
 
         return {
             statusCode: 200,
+            headers: {
+              "Content-Type": "application/json"  
+            },
             body: JSON.stringify({
                 url: result.Location
             })
@@ -26,6 +30,9 @@ export async function handler(event: APIGatewayEvent): Promise<APIGatewayProxyRe
     } catch (error: any) {
         return {
             statusCode: 500,
+            headers: {
+                "Content-Type": "application/json"  
+            },
             body: JSON.stringify({
                 error: error.message
             })
