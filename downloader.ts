c1c5b22482cd7173ab383b91f1e43f2329119cae
Leandro Lima <leandro.lima@oowlish.com>
2022-10-05 20:37:11 -0300

    moving video upload process to a function

--- downloader.ts
+++ downloader.ts
@@ -1,6 +1,6 @@
 import AWS from "aws-sdk";
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
-import ytdl from "ytdl-core";
+import ytdl, { videoInfo } from "ytdl-core";
 
 export async function handler(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
     const body = JSON.parse(event.body!);
@@ -9,19 +9,22 @@ export async function handler(event: APIGatewayEvent): Promise<APIGatewayProxyRe
     try {
         const videoInfo = await ytdl.getBasicInfo(videoUrl);
         const videoStream = await ytdl(videoUrl);
-
-        const s3Client = new AWS.S3();
-
-        const result = await s3Client.upload({
-            Bucket: process.env.VIDEOS_BUCKET_NAME!,
-            Key: `videos/${videoInfo.videoDetails.title}-${Date.now()}.mp4`,
-            Body: videoStream
-        }).promise();
+        const result = await uploadVideoToS3(videoInfo, videoStream);
 
         return getResponseObject(200, { url: result.Location });
     } catch (error: any) {
         return getResponseObject(500, { error: error.message });
     }
+}
+
+async function uploadVideoToS3(videoInfo: videoInfo, videoStream: AWS.S3.Body) {
+    const s3Client = new AWS.S3();
+
+    return s3Client.upload({
+        Bucket: process.env.VIDEOS_BUCKET_NAME!,
+        Key: `videos/${videoInfo.videoDetails.title}-${Date.now()}.mp4`,
+        Body: videoStream
+    }).promise();
 }
 
 function getResponseObject(statusCode: number, body: any) {
